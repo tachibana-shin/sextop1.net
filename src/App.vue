25452bf3e3e60e06d2d078fa3a046bff341ff7f2
nguyenthanh1995 <Thanhnguyennguyen1995@gmail.com>
2020-10-08 19:57:29 +0700

    .

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
    <div id="app">
-      <transition name="swipeY.top:ease">
-         <header-app @show-menu="NavListShow = true" v-show="HeaderAppShow" @input="HeaderAppShow = $event" class="header-app" />
-      </transition>
-      <transition name="fade:ease">
-         <backdrop-mark v-if="NavListShow" />
-      </transition>
-      <navbar-nav :class="{ active: NavListShow }" @hide-menu="NavListShow = false" />
+      <header-app/>
       <router-view class="router-view" />
       <footer-app />
    </div>
@@ -35,51 +29,11 @@
             }
          }
       }
-
-      .fade\:ease-enter-active,
-      .fade\:ease-leave-active {
-         transition: all .666s ease;
-      }
-
-      .fade\:ease-enter,
-      .fade\:ease-leave-to {
-         opacity: 0;
-      }
-
-      .swipeY\.top\:ease-enter-active {
-         animation: swipeYTopEnter .2s ease;
-
-         @keyframes swipeYTopEnter {
-            from {
-               transform: translateY(-100%);
-            }
-
-            to {
-               transform: translateY(0);
-            }
-         }
-      }
-
-      .swipeY\.top\:ease-leave-active {
-         animation: swipeYTopLeave .2s ease;
-
-         @keyframes swipeYTopLeave {
-            from {
-               transform: translateY(0);
-            }
-
-            to {
-               transform: translateY(-100%);
-            }
-         }
-      }
    }
 </style>
 <script>
    import HeaderApp from "./components/Header.vue"
-   import NavbarNav from "./components/NavBar-Nav.vue"
    import FooterApp from "./components/Footer.vue"
-   import BackdropMark from "./components/Backdrop-Mark.vue"
    
    // @import awesome
    import '@/awesome/scss/fontawesome.scss'
