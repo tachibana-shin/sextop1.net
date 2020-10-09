426595143f66d0c96ad30fbeae229bcb87f627e0
nguyenthanh1995 <Thanhnguyennguyen1995@gmail.com>
2020-10-09 16:53:45 +0700

    .

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const history = require("connect-history-api-fallback")
 const path = require("path")
 const serveStatic = require("serve-static")
 
@@ -9,22 +10,12 @@ const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000
 
+app.use(history())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(require("cors")())
 app.use(serveStatic(__dirname + "/dist"));
 
 router(app)
-
-app.use(function(req, res) {
-   res.status(404).send({
-      state: {
-         error: true,
-         code: 404,
-         message: "Not Found"
-      },
-      data: {}
-   })
-})
 
 app.listen(port, () => console.log(`App it running in port ${port}`))
